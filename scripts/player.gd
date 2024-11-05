47e7e0b8419a092868b12838e9c66408e859ff8f
russellchao <russchaoman@gmail.com>
2024-11-05 17:39:44 -0500

    Added collisions

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,31 @@
-extends Area2D
+extends CharacterBody2D
 
+var speed = 300
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var speed = 5
+func _physics_process(delta: float) -> void:	
+	
+	# Run 
 	if Input.is_action_pressed("run"):
-		speed = 10
+		speed = 600
+	else:
+		speed = 300
+	
+	# Horizontal Movement
 	if Input.is_action_pressed("move_right"):
-		position.x += speed
-	if Input.is_action_pressed("move_left"):
-		position.x -= speed
+		velocity.x = 1
+	elif Input.is_action_pressed("move_left"):
+		velocity.x = -1
+	else:
+		velocity.x = 0
+		
+	# Vertical Movement
 	if Input.is_action_pressed("move_down"):
-		position.y += speed
-	if Input.is_action_pressed("move_up"):
-		position.y -= speed
+		velocity.y = 1
+	elif Input.is_action_pressed("move_up"):
+		velocity.y = -1
+	else:
+		velocity.y = 0
+	
+	# Apply Movement and Collisions
+	velocity = velocity * speed
+	move_and_slide()
