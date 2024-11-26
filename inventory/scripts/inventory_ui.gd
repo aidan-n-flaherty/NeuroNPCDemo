be1f4ca59a4c5f8c6db4c30f7d4a967bf170758d
russellchao <russchaoman@gmail.com>
2024-11-26 16:24:15 -0500

    Fixed Inventory Size

--- inventory/scripts/inventory_ui.gd
+++ inventory/scripts/inventory_ui.gd
@@ -1,9 +1,17 @@
 extends Control
 
+@onready var inv: Inv = preload("res://inventory/playerInventory.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
 var is_open = false
 
 func _ready():
+	update_slots()
 	close()
+	
+func update_slots():
+	for i in range(min(inv.items.size(), slots.size())):
+		slots[i].update(inv.items[i])
 	
 func _process(delta):
 	if Input.is_action_just_pressed("open_inventory"):
--- scripts/npc_brain.gd
+++ scripts/npc_brain.gd
@@ -5,15 +5,6 @@ var target: Vector3
 func init(id: int, artificial=true):
 	super.init(id, artificial)
 
-func processCommand(agentID: int, action: Action):
-	if agentID != self.id:
-		return
-	
-	if action.actionType == 'say':
-		get_parent().displaySpeechBubble(action.parameters[0])
-	if action.actionType == 'attack':
-		attacking = AgentManager.getAgent(action.parameters[0])
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
